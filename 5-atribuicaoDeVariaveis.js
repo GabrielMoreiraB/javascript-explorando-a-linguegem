eb23a35f88aec021db257fd962b3a7cf2ecfc138
Gabriel Barbosa Moreira <109036024+GabrielMoreiraB@users.noreply.github.com>
2023-01-10 17:54:42 -0300

    ,

---
+++ 5-atribuicaoDeVariaveis.js
@@ -0,0 +1,8 @@
+console.log("Trabalhando com Atribuição de Variaveis");
+
+const idade = 29;
+const nome = "Ricardo";
+const sobrenome = "Bugan";
+
+console.log(nome, sobrenome);
+console.log(`meu nome é ${nome} ${sobrenome}`);
