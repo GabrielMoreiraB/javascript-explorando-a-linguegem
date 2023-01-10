088e2854a6099d5e85a59a415db7afb722a23936
Gabriel Barbosa Moreira <109036024+GabrielMoreiraB@users.noreply.github.com>
2023-01-10 19:05:48 -0300

    Create 7-condicionais.js

---
+++ 7-condicionais.js
@@ -0,0 +1,17 @@
+console.log(`Trabanhando com Condicionais`);
+const listaDeDestinos = new Array(
+    `Salvador`, 
+    `São Paulo`, 
+    `Rio de Janeiro`
+
+);
+
+
+console.log("Destinos Possiveis");
+
+console.log(listaDeDestinos);
+
+//remorver
+listaDeDestinos.splice(1, 1);
+
+console.log(listaDeDestinos);
