6abe4178697129028a82f1e96d30f64f6a5e5ab5
Gabriel Barbosa Moreira <109036024+GabrielMoreiraB@users.noreply.github.com>
2023-01-16 16:51:18 -0300

    ,

--- projeto/index.js
+++ projeto/index.js
@@ -1,13 +1,10 @@
 import { cliente } from "./Cliente.js";
 import { contaCorrente } from "./ContaCorrente.js";
 
-const cliente1 = new cliente();
-    cliente1.nome = "Ricardo";
-    cliente1.cpf = 11122233309;
+const cliente1 = new cliente("Ricardo", 11122233309);
 
-const cliente2 = new cliente();
-    cliente2.nome = "Alice";
-    cliente2.cpf = 88899977730;
+
+const cliente2 = new cliente("Alice", 88899977730);
 
 const contaCorrenteRicardo = new contaCorrente();
     contaCorrenteRicardo.agencia = 1001;
